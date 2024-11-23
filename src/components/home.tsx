aefc9d53d9775f6597d12e40dd31351350c1e06f
James Kalu <jamesiukalu@gmail.com>
2024-11-22 19:13:47 -0500

    fix

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
-    <div className="w-screen h-screen">
+    <div className="w-screen h-screen flex items-center justify-center">
+      <Link 
+        to="/product" 
+        className="text-blue-600 hover:text-blue-800 underline"
+      >
+        View Product Page
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
